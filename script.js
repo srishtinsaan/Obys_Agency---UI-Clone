5f721a1703209c2f75bfdb31e2190bf35fa04642
Srishti Singh <srish17816@gmail.com>
2025-05-16 08:46:09 +0530

    page4done

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-function cursorAnimation(){
-    document.addEventListener("mousemove",function(dets){
-    gsap.to(`#cursor`,{
-        left: dets.x,
-        top: dets.y
-    })
-})
+function loco(){
+    gsap.registerPlugin(ScrollTrigger);
 
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
+function cursorAnimation(){
+    const moveCursorX = gsap.quickTo("#cursor", "left", { duration: 0.2, ease: "power3" });
+const moveCursorY = gsap.quickTo("#cursor", "top", { duration: 0.2, ease: "power3" });
+
+document.addEventListener("mousemove", (e) => {
+    moveCursorX(e.clientX);
+    moveCursorY(e.clientY);
+});
 }
 
 function loadingAnimation(){
@@ -61,7 +94,7 @@ tl.to(`#loader`,{
     display:`none`
 })
 
-tl.from(`#hero1 h1, #hero2 h1, #hero3 h2,#hero4 h1`,{
+tl.from(`#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero4 h1`,{
     y:120,
     stagger:0.1
 })
@@ -80,8 +113,30 @@ function magnetAnimation(){
     });
 }
 
+function cursor2_Animation(){
+    const moveCursorX = gsap.quickTo("#cursor2", "left", { duration: 0.2, ease: "power3" });
+    const moveCursorY = gsap.quickTo("#cursor2", "top", { duration: 0.2, ease: "power3" });
+    let container = document.querySelector(`video1`)
+    container.addEventListener("mousemove", (e) => {
+    moveCursorX(e.clientX);
+    moveCursorY(e.clientY);
+});
+}
 
+function playvideo(){
+    let icon = document.querySelector(`#cursor2 i`);
+    let video = document.querySelector(`#video1 video`);
+    let overlayimage = document.querySelector(`#video1 img`)
+    icon.addEventListener( `onclick`, function(){
+        overlayimage.style.opacity = "0"; 
+        video.play()
+    })
+}
+
+loco()
 loadingAnimation()
+playvideo()
+cursorAnimation()
 cursorAnimation()
 magnetAnimation()
 
